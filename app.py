74c0edafc64eadc42e1ebdbff59894cd29f59f65
cc090206 <chowchloe5@gmail.com>
2025-04-30 02:33:00 -0700

    Started application

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
